e7f91d0413b0e8f462ecebffd31da132740f252b
Benjamin Schmid <git@schmidb.ch>
2019-02-22 20:08:23 +0100

    initial commit

---
+++ android/src/com/smeanox/games/aj05/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.smeanox.games.aj05;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.smeanox.games.aj05.AJ05;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AJ05(), config);
+	}
+}
---
+++ desktop/src/com/smeanox/games/aj05/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smeanox.games.aj05.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smeanox.games.aj05.AJ05;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AJ05(), config);
+	}
+}
